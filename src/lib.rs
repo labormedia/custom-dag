936e13cf939204c4ee640d51b71a5611b2ef0bed
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-03-04 18:15:41 +0000

    Generic type for Node<T> and Dag<T> (cascade).

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,18 @@
 use std::collections::HashSet;
+use core::hash::Hash;
 
 type BlockId = u32;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Node {
-    id: BlockId,
-    left: Option<BlockId>,
-    right: Option<BlockId>,
+pub struct Node<T: Eq + Hash + PartialEq> {
+    id: T,
+    left: Option<T>,
+    right: Option<T>,
 }
 
-impl Node {
+impl<T: Eq + Hash + PartialEq> Node<T> {
     // The Node itself can be self-referential and makes no assumptions about the structure of the graph.
-    pub fn new(id: BlockId, left: Option<BlockId>, right: Option<BlockId>) -> Self {
+    pub fn new(id: T, left: Option<T>, right: Option<T>) -> Self {
         Node {
             id,
             left,
@@ -21,17 +22,17 @@ impl Node {
 }
 
 #[derive(Debug, Clone)]
-pub struct Dag {
-    nodes: HashSet<Node>,
+pub struct Dag<T: Eq + Hash + PartialEq> {
+    nodes: HashSet<Node<T>>,
 }
 
-impl Dag {
+impl<T: Eq + Hash + PartialEq> Dag<T> {
     pub fn new() -> Self {
         Dag {
             nodes: HashSet::new()
         }
     }
-    pub fn insert(&mut self, node: Node) {
+    pub fn insert(&mut self, node: Node<T>) {
         self.nodes.insert(node);
     }
 }
@@ -54,7 +55,7 @@ mod tests {
     }
 
     #[test]
-    fn create_node_zero() {
+    fn create_node_zero_u32() {
         let node = Node::new(0,Some(0),Some(0));
         assert_eq!(
             node, 
@@ -65,4 +66,33 @@ mod tests {
             }
         )
     }
+
+    #[test]
+    fn create_node_zero_u64() {
+        let value = 0_u64;
+        let node = Node::new(value,Some(0),Some(0));
+        assert_eq!(
+            node, 
+            Node {
+                id: 0,
+                left:Some(0),
+                right: Some(0),
+            }
+        )
+    }
+
+    #[test]
+    fn create_node_zero_usize() {
+        let value = 0_usize;
+        let node = Node::new(value,Some(0),Some(0));
+        assert_eq!(
+            node, 
+            Node {
+                id: 0,
+                left:Some(0),
+                right: Some(0),
+            }
+        )
+    }
+    
 }
