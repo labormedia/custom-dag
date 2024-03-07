6ed19edd93526bcb2dbc8ea1bafafc3f3f0dfe7f
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-03-07 17:15:10 +0000

    Edits ingtegration tests comments

--- tests/dag.rs
+++ tests/dag.rs
@@ -8,9 +8,8 @@ use custom_dag::{
     collitions::CollidingNode,
 };
 // randomness is added to the integration tests to assert the validity of nodes that could contain any value within the range of the id type.
-// This means that the tests using the random generator are not deterministic. This is why we include them as integration tests.
-// It is necessary to ackownledge, then, that a complete formal verification should include all the values of the id type set.
-// In favor of performance, probabilistic testing for large number of nodes is prioritized first.
+// This means that the tests using the random generator are not deterministic.
+// A complete formal verification should include all the values of the id type set.
 use rand::Rng;
 
 #[test]
