7f0743120649117b171c4340910d7472f09f5e6c
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-03-09 01:20:19 +0000

    Adds edge_sum() to Topology. Adds test insert_nodes_in_topology_analysis().

--- src/collitions.rs
+++ src/collitions.rs
@@ -4,7 +4,7 @@ use crate::Node;
 /// `CollidingNode<T>` is a struct wrapper similar and exchangeable to `Node<T>`. Whereas normal `Node<T>`
 /// are defined to be equal if the id is equal, `CollingNode<T>` will be different if *any* field of the node is different.
 #[derive(Debug, Clone, Hash, Copy, PartialEq, Eq)]
-pub struct CollidingNode<T: Eq + Hash + PartialEq + Copy>(Node<T>);
+pub struct CollidingNode<T: Eq + Hash + PartialEq + Copy>(pub Node<T>);
 
 impl<T: Eq + Hash + PartialEq + Copy> From<Node<T>> for CollidingNode<T> {
     fn from(node: Node<T>) -> Self  {
--- src/topological.rs
+++ src/topological.rs
@@ -33,7 +33,7 @@ impl fmt::Display for TopologicalError {
 
 /// Topology struct layout for analysis.
 #[derive(Debug, Clone)]
-struct Topology<T: Eq + Hash + PartialEq + Copy + std::fmt::Debug> {
+pub struct Topology<T: Eq + Hash + PartialEq + Copy + std::fmt::Debug> {
     all_nodes: HashSet<CollidingNode<T>>, // collection of all nodes compared by all fields
     unique_nodes: HashSet<Node<T>>,
     collitions: HashSet<CollidingNode<T>>,
@@ -51,22 +51,27 @@ impl<T: Eq + Hash + PartialEq + Copy + std::fmt::Debug> Topology<T> {
             edges: HashMap::new(),
         }
     }
-    fn insert(&mut self, node: Node<T>) -> Result<Node<T>, TopologicalError>{
+    /// Inserts the node into the topology analysis.
+    /// If it finds a collition, returns an Option<CollidingNode<T>> with the value of the node.
+    /// otherwise it returns None if there is no collision.
+    fn insert(&mut self, node: Node<T>) -> Option<CollidingNode<T>>{
         if self.all_nodes.insert(node.into()) { // if the node didn't existed in the collection of all nodes (compared by all fields)
             if self.unique_nodes.insert(node) { // if the node didn't existed in the collection of unique nodes (compared by id)
                 self.collect_edges(node); // collect the node's edges
+                None
             } else { // else if the node was already inserted in the collection of unique nodes (compared by id)
-                if self.collitions.insert(node.into()) { // when inserting the node to the collection of collitions, if the node was not listed there
+                if self.collitions.insert(node.into()) { // insert the node to the collection of collitions; if the node was not listed there
                     // done
                 } else {
                     self.collect_repeated_node(node); // also collect the node as a repeated_node
                 };
-            };
+                Some(node.into())  // return the colliding node.
+            }
         } else { // else if the node existed in the collection of all nodes (compared by all fields)
             assert!(self.unique_nodes.insert(node));  // It should have been already added to the collection of unique nodes (compared by id).
             self.collect_repeated_node(node); // and collects it to the collection of repeated nodes.
-        };
-        Ok(node)
+            Some(node.into()) // return the colliding node
+        }
     }
     fn collect_repeated_node(&mut self, node: Node<T>) {
         if let Some(repeated_nodes_set) = self.repeated_nodes.get_mut(&node.id) { // if the set of repeated nodes is already created for this node.id
@@ -99,6 +104,13 @@ impl<T: Eq + Hash + PartialEq + Copy + std::fmt::Debug> Topology<T> {
             },
         };
     }
+    fn edge_sum(&self) -> usize {
+        self
+            .edges
+            .iter()
+            .map(|(from, list)| { list.len() })
+            .sum()
+    } 
     fn sort(nodes:&[CollidingNode<T>]) -> Result<Topology<T>, TopologicalError> {
         let topology: Topology<T> = Topology::new();
         for nodes in nodes.iter() {
@@ -107,4 +119,32 @@ impl<T: Eq + Hash + PartialEq + Copy + std::fmt::Debug> Topology<T> {
         Ok::<Topology<T>, TopologicalError>(topology);
         Err(TopologicalError::Custom)
     }
+}
+
+#[test]
+fn insert_nodes_in_topology_analysis() {
+    let node_a = Node::new(0,None,None);
+    let node_b = Node::new(1,Some(0),None);
+    let node_c = Node::new(2,None,Some(0));
+    let node_d = Node::new(3,Some(0), Some(1));
+    let node_e = Node::new(4,Some(2), Some(1));
+    let node_f = Node::new(5,Some(3), Some(4));
+    let mut topology = Topology::new();
+    let node_list = [node_a, node_b, node_c, node_d, node_e, node_f];
+    for node in node_list.into_iter() {
+        topology.insert(node);
+    };
+    assert_eq!(topology.all_nodes.get(&Node::new(23, Some(42), Some(50)).into()), None);  // tries to take an inexistent node
+    for node in node_list.into_iter() {
+        assert_eq!(topology.all_nodes.get(&node.into()).expect("Wrong value assumption."), &CollidingNode(node));
+    };
+    for node in node_list.into_iter() {
+        assert_eq!(topology.unique_nodes.get(&node.into()).expect("Wrong value assumption."), &node);
+    };
+    assert_eq!(topology.collitions.len(), 0);
+    assert_eq!(topology.repeated_nodes.len(), 0);
+    assert_eq!(topology.edge_sum(), 8);
+    // for (from, to) in topology.edges.iter() {
+    //     topology.unique_nodes.get
+    // }
 }
