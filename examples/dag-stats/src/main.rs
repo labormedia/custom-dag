d2deb0328822d4b4aea73d7239a0e699df2a45ec
Diego Correa Tristain <algoritmia@labormedia.cl>
2024-03-12 00:03:07 +0000

    Adds new methods to example dag-stats.

--- examples/dag-stats/src/main.rs
+++ examples/dag-stats/src/main.rs
@@ -1,13 +1,32 @@
-use std::env;
-use std::fs;
+use std::{
+    env,
+    fs
+};
+use custom_dag::{
+    Node,
+    topological::Topology
+};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let file_path = &args[1];
-    // println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path)
         .expect("Should have been able to read the file");
 
-    // println!("With text:\n{contents}");
+    let mut enumerated_lines = contents.lines().enumerate();
+    let dag_size: usize = enumerated_lines.next().expect("Wrong file format.").1.parse().expect("Wrong file format.");
+    let mut nodes_vector: Vec<Node<u32>> = Vec::with_capacity(dag_size);
+
+    nodes_vector.push(Node::new(1, None, None));
+    for (i,node_data) in enumerated_lines {
+        let Some((left, right)) = node_data.split_once(" ") else { panic!("Wrong file format.") };
+        nodes_vector.push(Node::new((i + 1) as u32, Some(left.parse().expect("Wrong file format.")), Some(right.parse().expect("Wrong file format."))));
+    }
+
+    println!("Nodes vector : {nodes_vector:?}");
+    let Ok(Some(sorted)) = Topology::sort(&nodes_vector) else { panic!("Wrong topological assumptions for this test data.") };
+    let Ok(Some(shortest_and_longest)) = Topology::shortest_and_longest_paths(&sorted) else { panic!("Wrong topological assumptions for this test data.") };
+    let printable: Vec<(&u32, &(Option<usize>, Option<isize>))> = shortest_and_longest.iter().collect();
+    println!("another shortest and longest : {:?}", printable);
 }
